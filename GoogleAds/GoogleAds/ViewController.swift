d38351d52533c125b5737294417f9348be104be0
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2018-11-14 11:11:36 +0200

    start project

---
+++ GoogleAds/GoogleAds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleAds
+//
+//  Created by Sergey Pogrebnyak on 14.11.2018.
+//  Copyright © 2018 Sergey Pogrebnyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
